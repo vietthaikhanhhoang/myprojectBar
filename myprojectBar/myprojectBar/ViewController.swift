7727e45dfa19d81d052865dab51f36f6aac7fb40
Ba Viet <vietnbctv@gmail.com>
2021-07-02 14:02:45 +0700

    add

---
+++ myprojectBar/myprojectBar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myprojectBar
+//
+//  Created by Nguyen Ba Viet on 02/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
